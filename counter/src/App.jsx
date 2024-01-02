dafa33baedb00c7352cacb0094f2a868c2d9ceb3
Shankar-001 <praveen01.ugec20@iiitranchi.ac.in>
2024-01-02 20:38:47 +0530

    counter currencyConvertor passGenerator

---
+++ counter/src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  let [counter, setCounter] = useState(0);
+  // let counter = 0;
+
+  const increaseValue = () => {
+    if(counter<20) {
+      setCounter(counter+1);
+    }
+  };
+
+  const decreaseValue = () => {
+    if (counter > 0) {
+      setCounter(counter-1);
+    }
+  };
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button onClick={increaseValue}>Increment</button>
+      <button onClick={decreaseValue}>Decrement</button>
+    </>
+  );
+}
+
+export default App;

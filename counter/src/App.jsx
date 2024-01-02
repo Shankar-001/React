a01b7a61be0f00ae13ce7c50a4b5af82a5e2eb7b
Shankar-001 <praveen01.ugec20@iiitranchi.ac.in>
2024-01-02 21:05:59 +0530

    using props in counter

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -5,15 +5,24 @@ function App() {
   let [counter, setCounter] = useState(0);
   // let counter = 0;
 
-  const increaseValue = () => {
-    if(counter<20) {
-      setCounter(counter+1);
-    }
-  };
+  // const increaseValue = () => {
+  //   if(counter<20) {
+  //     setCounter(counter+1);
+  //   }
+  // };
 
-  const decreaseValue = () => {
-    if (counter > 0) {
-      setCounter(counter-1);
+  // const decreaseValue = () => {
+  //   if (counter > 0) {
+  //     setCounter(counter-1);
+  //   }
+  // };
+
+  const changeValue = (val) => {
+    if (counter + val > 20 || counter + val < 0) {
+      setCounter(counter);
+    }
+    else {
+      setCounter(counter + val);
     }
   };
 
@@ -21,8 +30,8 @@ function App() {
     <>
       <h1>Hello</h1>
       <h2>Counter Value: {counter}</h2>
-      <button onClick={increaseValue}>Increment</button>
-      <button onClick={decreaseValue}>Decrement</button>
+      <button onClick={() => changeValue(3)}>Increment</button>
+      <button onClick={() => changeValue(-3)}>Decrement</button>
     </>
   );
 }

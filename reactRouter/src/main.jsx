8b8a0b7abdc3db13fcaf810d77c778474969bd19
Shankar-001 <praveen01.ugec20@iiitranchi.ac.in>
2024-01-03 20:44:11 +0530

    react-router

---
+++ reactRouter/src/main.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App.jsx'
+import About from './components/About/About.jsx'
+import Home from './components/Home/Home.jsx'
+import './index.css'
+import Layout from './Layout.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
